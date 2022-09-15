1b8fc0f81c114b620d5eae84ffaf296a223d07e1
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-15 16:59:59 -0300

    css product

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -30,6 +30,21 @@ const Product = () => {
             <span className={styles.price}>${pizza.price[size]}</span>
             <p className={styles.desc}>{pizza.desc}</p>
             <h3 className={styles.choose}>Choose the size</h3>
+
+            <div className={styles.sizes}>
+               <div className={styles.size}>
+                  <Image alt="" src="/img/size.png" layout="fill" />
+                  <span className={styles.number}>Small</span>
+               </div>
+               <div className={styles.size}>
+                  <Image alt="" src="/img/size.png" layout="fill" />
+                  <span className={styles.number}>Medium</span>
+               </div>
+               <div className={styles.size}>
+                  <Image alt="" src="/img/size.png" layout="fill" />
+                  <span className={styles.number}>Large</span>
+               </div>
+            </div>
          </div>
       </div>
    );

5a5485c0adf232aaa2f06b3abccc9626a9af33e3
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-15 11:06:58 -0300

    Featured

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import styles from "../styles/Featured.module.css";
 
+const images = [
+   "/img/featured.png",
+   "/img/featured2.png",
+   "/img/featured3.png",
+];
+
 const Featured = () => {
+   const [indexImg, setIndexImg] = useState(0);
+
+   const handleArrow = (direction) => {
+      if (direction === "l") {
+         setIndexImg(indexImg !== 0 ? indexImg - 1 : 2);
+      }
+      if (direction === "r") {
+         setIndexImg(indexImg !== 2 ? indexImg + 1 : 0);
+      }
+   };
+
    return (
       <div className={styles.container}>
-         <Image src="/img/"/>
+         <div
+            className={styles.arrowContainer}
+            style={{ left: 0 }}
+            onClick={() => handleArrow("l")}
+         >
+            <Image
+               src="/img/arrowl.png"
+               alt=""
+               layout="fill"
+               objectFit="contian"
+            />
+         </div>
+
+         <div
+            className={styles.wrapper}
+            style={{
+               transform: `translateX(${-100 * indexImg}vw)`,
+            }}
+         >
+            {images.map((img, index) => (
+               <div key={index} className={styles.imgContainer}>
+                  <Image src={img} alt="" layout="fill" objectFit="contian" />
+               </div>
+            ))}
+         </div>
+
+         <div
+            className={styles.arrowContainer}
+            style={{ right: 0 }}
+            onClick={() => handleArrow("r")}
+         >
+            <Image
+               src="/img/arrowr.png"
+               alt=""
+               layout="fill"
+               objectFit="contian"
+            />
+         </div>
       </div>
    );
 };
---
+++ components/PizzaList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "../styles/PizzaList.module.css";
+
+const PizzaList = () => {
+   return (
+      <div className={styles.container}>
+         <h1 className={styles.title}></h1>
+         <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dolor
+            sapiente quas doloremque aspernatur, beatae voluptates recusandae
+            nobis tempore ipsam, laborum soluta quae illo maxime cupiditate
+            illum molestias porro eveniet!
+         </p>
+         <div className={styles.wrapper}>
+            
+         </div>
+      </div>
+   );
+};
+
+export default PizzaList;

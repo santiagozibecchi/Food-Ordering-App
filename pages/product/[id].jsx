273192b094fc968091d28fd8f7a1c7c6041319d6
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-15 17:01:59 -0300

    cambiando el estado del precio

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -32,15 +32,15 @@ const Product = () => {
             <h3 className={styles.choose}>Choose the size</h3>
 
             <div className={styles.sizes}>
-               <div className={styles.size}>
+               <div className={styles.size} onClick={() => setsize(0)}>
                   <Image alt="" src="/img/size.png" layout="fill" />
                   <span className={styles.number}>Small</span>
                </div>
-               <div className={styles.size}>
+               <div className={styles.size} onClick={() => setsize(1)}>
                   <Image alt="" src="/img/size.png" layout="fill" />
                   <span className={styles.number}>Medium</span>
                </div>
-               <div className={styles.size}>
+               <div className={styles.size} onClick={() => setsize(2)}>
                   <Image alt="" src="/img/size.png" layout="fill" />
                   <span className={styles.number}>Large</span>
                </div>

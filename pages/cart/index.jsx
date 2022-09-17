3919ddf215474f399413c0f523464f27a63030c3
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-17 17:25:21 -0300

    mostrando boton de pago con paypay

--- pages/cart/index.jsx
+++ pages/cart/index.jsx
@@ -19,6 +19,57 @@ const CartPage = () => {
    const dispatch = useDispatch();
    const cart = useSelector((state) => state.cart);
 
+   // Custom component to wrap the PayPalButtons and handle currency changes
+   const ButtonWrapper = ({ currency, showSpinner }) => {
+      // usePayPalScriptReducer can be use only inside children of PayPalScriptProviders
+      // This is the main reason to wrap the PayPalButtons in a new component
+      const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+
+      useEffect(() => {
+         dispatch({
+            type: "resetOptions",
+            value: {
+               ...options,
+               currency: currency,
+            },
+         });
+      }, [currency, showSpinner]);
+
+      return (
+         <>
+            {showSpinner && isPending && <div className="spinner" />}
+            <PayPalButtons
+               style={style}
+               disabled={false}
+               forceReRender={[amount, currency, style]}
+               fundingSource={undefined}
+               createOrder={(data, actions) => {
+                  return actions.order
+                     .create({
+                        purchase_units: [
+                           {
+                              amount: {
+                                 currency_code: currency,
+                                 value: amount,
+                              },
+                           },
+                        ],
+                     })
+                     .then((orderId) => {
+                        // Your code here after create the order
+                        return orderId;
+                     });
+               }}
+               onApprove={function (data, actions) {
+                  return actions.order.capture().then(function () {
+                     // Your code here after capture the order
+                  });
+               }}
+            />
+         </>
+      );
+   };
+
    return (
       <div className={styles.container}>
          <div className={styles.left}>
@@ -90,6 +141,16 @@ const CartPage = () => {
                   <b className={styles.totalTextTitle}>Total:</b>${cart.total}
                </div>
                <button className={styles.button}>CHECKOUT NOW!</button>
+               <PayPalScriptProvider
+                  options={{
+                     "client-id": "test",
+                     components: "buttons",
+                     currency: "USD",
+                     "disable-funding": "credit,card,p24",
+                  }}
+               >
+                  <ButtonWrapper currency={currency} showSpinner={false} />
+               </PayPalScriptProvider>
             </div>
          </div>
       </div>

5c239b00f3fa8baaa64fbaaf5c038bda7885c017
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-18 15:18:13 -0300

    pasando product y order por props a los componentes

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Image from "next/image";
 import styles from "../../styles/Admin.module.css";
+import axios from "axios";
 
-const AdminPage = () => {
+const AdminPage = ({ orders, products }) => {
+   console.log(products);
    return (
       <div className={styles.container}>
+         {/* PRODUCTS */}
          <div className={styles.item}>
             <h1 className={styles.title}>Products</h1>
 
@@ -18,28 +21,31 @@ const AdminPage = () => {
                      <th>Action</th>
                   </tr>
                </thead>
-               <tbody>
-                  <tr className={styles.trTitle}>
-                     <td>
-                        <Image
-                           src="/img/pizza.png"
-                           width={50}
-                           height={50}
-                           objectFit="cover"
-                           alt=""
-                        />
-                     </td>
-                     <td>PizzaId</td>
-                     <td>PizzaTitle</td>
-                     <td>$50</td>
-                     <td>
-                        <button className={styles.button}>Edit</button>
-                        <button className={styles.button}>Delete</button>
-                     </td>
-                  </tr>
-               </tbody>
+               {products.map((product) => (
+                  <tbody key={product._id}>
+                     <tr className={styles.trTitle}>
+                        <td>
+                           <Image
+                              src={product.img}
+                              width={50}
+                              height={50}
+                              objectFit="cover"
+                              alt=""
+                           />
+                        </td>
+                        <td>{product._id.slice(0, 5)}...</td>
+                        <td>{product.title}</td>
+                        <td>${product.prices[0]}</td>
+                        <td>
+                           <button className={styles.button}>Edit</button>
+                           <button className={styles.button}>Delete</button>
+                        </td>
+                     </tr>
+                  </tbody>
+               ))}
             </table>
          </div>
+         {/* ORDERS */}
          <div className={styles.item}>
             <h1 className={styles.title}>Orders</h1>
 
@@ -56,7 +62,7 @@ const AdminPage = () => {
                </thead>
                <tbody>
                   <tr className={styles.trTitle}>
-                     <td>{"5666612318041".slice(0,5)}...</td>
+                     <td>{"5666612318041".slice(0, 5)}...</td>
                      <td>John Doe</td>
                      <td>$50</td>
                      <td>Paid</td>
@@ -70,6 +76,18 @@ const AdminPage = () => {
          </div>
       </div>
    );
+};
+
+export const getServerSideProps = async () => {
+   const productsRes = await axios.get("http://localhost:3000/api/products");
+   const orderRes = await axios.get("http://localhost:3000/api/orders");
+
+   return {
+      props: {
+         orders: orderRes.data,
+         products: productsRes.data,
+      },
+   };
 };
 
 export default AdminPage;
